cb6b174a62abf10f086e50a4d538d3be5a8f6ca1
laucavv <laucavv23@gmail.com>
2020-11-24 10:36:03 -0500

    add message model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
+"""Class Message """
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+
+    """ configuration of the properties of a message """
+
+    id = models.AutoField(primary_key=True)
+    message = models.CharField(max_length=1200)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    delete = models.BooleanField(default=False)
+    image = models.FileField(upload_to='documents/')
+
