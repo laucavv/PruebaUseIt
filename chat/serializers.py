43c795913d4c7bdd8fc6fadc1fc5e3ab44b7bafe
laucavv <laucavv23@gmail.com>
2020-11-24 12:43:48 -0500

    change line 22 objet a object

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,25 +1,25 @@
 
-""" serialization of models """"
+""" serialization of models """
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from chat.models import Message
 
 class SerializerUser(serializers.ModelSerializer):
-    " serialization of User "
+    """ serialization of User """
 
     password = serializers.CharField(write_only=True)
 
     class Meta:
-    model = User
-    field = ['username', 'password']
+        model = User
+        field = ['username', 'password']
 
 
 class SerializerMessage(serializers.ModelSerializer):
-    " serialization of Message "
+    """ serialization of Message """
 
     id = serializers.ReadOnlyField(required=False)
-    sender = serializers.SlugRelatedField(many=False, slug_field='username', queryset=User.objets.all())
-    receiver = serializers.SlugRelatedField(many=False, slug_field='username', queryset=User.objets.all())
+    sender = serializers.SlugRelatedField(many=False, slug_field='username', queryset=User.objects.all())
+    receiver = serializers.SlugRelatedField(many=False, slug_field='username', queryset=User.objects.all())
     delete = serializers.BooleanField(required=False)
 
     class Meta:
