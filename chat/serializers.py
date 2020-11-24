6ed8159363d5772b45e89fc90421e715985dd82d
laucavv <laucavv23@gmail.com>
2020-11-24 11:07:08 -0500

    add seializers of user and message

---
+++ chat/serializers.py
@@ -0,0 +1,27 @@
+
+""" serialization of models """"
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from chat.models import Message
+
+class SerializerUser(serializers.ModelSerializer):
+    " serialization of User "
+
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+    model = User
+    field = ['username', 'password']
+
+
+class SerializerMessage(serializers.ModelSerializer):
+    " serialization of Message "
+
+    id = serializers.ReadOnlyField(required=False)
+    sender = serializers.SlugRelatedField(many=False, slug_field='username', queryset=User.objets.all())
+    receiver = serializers.SlugRelatedField(many=False, slug_field='username', queryset=User.objets.all())
+    delete = serializers.BooleanField(required=False)
+
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'receiver', 'message', 'delete']
